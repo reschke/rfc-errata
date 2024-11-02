4c9b37a124fdd69bd993e7cb0680e24e643fdb83
Kesara Rathnayake <kesara@fq.nz>
2024-11-02 15:48:34 +0000

    Use GHA to update erratas

--- Makefile
+++
@@ -1,7 +0,0 @@
-all:	errata.json
-
-errata.json:
-	git pull
-	curl https://www.rfc-editor.org/errata.json -o errata.json.raw --etag-compare errata.json.etag --etag-save errata.json.etag
-	jq "sort_by(.errata_id)" errata.json.raw > $@
-	git commit -a -m "auto update" && git push && sh ./extract-by-rfc.sh $@
--- extract-by-rfc.sh
+++ extract-by-rfc.sh
@@ -2,9 +2,6 @@
 
 dirname=errata-by-rfc
 
-# refresh from repo
-git pull
-
 # reset all files (a DOCID may have disappeared when errata spam is deleted)
 for i in $dirname/rfc*json
 do
@@ -17,10 +14,4 @@ do
   num=$(echo $rfc | cut -c4-)
   fn=$dirname/rfc$num.json
   jq "[.[] | select(.\"doc-id\" == \"$rfc\")]" $1 > $fn
-  git add $fn || echo "$num already present"
-  git commit $fn -m "update rfc$num" || echo "$num unchanged"
 done
-
-# write back
-git commit $dirname/rfc*json -m "auto update" || echo unchanged
-git push || echo "nothing to commit"
